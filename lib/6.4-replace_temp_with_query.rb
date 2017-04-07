b6426b284999d432c9cd612aded8e095178deb0d
yamotonalds <yamotonalds@gmail.com>
2017-04-07 21:37:40 +0900

    Inline temp

--- lib/6.4-replace_temp_with_query.rb
+++ lib/6.4-replace_temp_with_query.rb
@@ -6,7 +6,7 @@ class ReplaceTempWithQuery
 
   def price
     a_base_price = base_price
-    if a_base_price > 1000
+    if base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
