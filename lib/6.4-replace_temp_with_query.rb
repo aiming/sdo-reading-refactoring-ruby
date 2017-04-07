8e351081d0cd59bafd87962eab480980c1392d8f
yamotonalds <yamotonalds@gmail.com>
2017-04-07 21:39:13 +0900

    Inline temp and remove temp variable

--- lib/6.4-replace_temp_with_query.rb
+++ lib/6.4-replace_temp_with_query.rb
@@ -5,13 +5,12 @@ class ReplaceTempWithQuery
   end
 
   def price
-    a_base_price = base_price
     if base_price > 1000
       discount_factor = 0.95
     else
       discount_factor = 0.98
     end
-    a_base_price * discount_factor
+    base_price * discount_factor
   end
 
   private
