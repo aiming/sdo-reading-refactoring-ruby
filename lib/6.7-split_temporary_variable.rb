f00c4f9e7989dd4beb654d16aff5f0b67c2d87c3
tkageyama <tkageyama@OSAKA-P-0398.local>
2017-04-20 20:05:21 +0900

    Extract calculate distance method

--- lib/6.7-split_temporary_variable.rb
+++ lib/6.7-split_temporary_variable.rb
@@ -9,13 +9,19 @@ class SplitTemporaryVariable
   def distance_traveled(time)
     primary_acc = @primary_force / @mass
     primary_time = [time, @delay].min
-    result = 0.5 * primary_acc * primary_time * primary_time
+    result = calculate_distance(primary_acc, primary_time)
     secondary_time = time - @delay
-    if (secondary_time > 0)
+    if secondary_time > 0
       primary_vel = primary_acc * @delay
       secondary_acc = (@primary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + 0.5 * secondary_acc * secondary_time * secondary_time
+      result += primary_vel * secondary_time + calculate_distance(secondary_acc, secondary_time)
     end
     result
+  end
+
+  private
+
+  def calculate_distance(acc, time)
+    0.5 * acc * time * time
   end
 end
