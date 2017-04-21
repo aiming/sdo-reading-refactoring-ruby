866b32105e321c0a5d56feb2d15529b698dce612
tkageyama <tkageyama@OSAKA-P-0398.local>
2017-04-21 09:59:09 +0900

    to be continue

--- lib/6.7-split_temporary_variable.rb
+++ lib/6.7-split_temporary_variable.rb
@@ -1,27 +1,26 @@
 class SplitTemporaryVariable
   def initialize(primary_force, mass, delay, secondary_force)
-    @primary_force = primary_force
-    @mass = mass
+    @primary_acc = primary_force / mass
     @delay = delay
-    @secondary_force = secondary_force
+    @secondary_acc = (primary_force + secondary_force) / mass
   end
 
   def distance_traveled(time)
-    primary_acc = @primary_force / @mass
-    primary_time = [time, @delay].min
-    result = calculate_distance(primary_acc, primary_time)
-    secondary_time = time - @delay
-    if secondary_time > 0
-      primary_vel = primary_acc * @delay
-      secondary_acc = (@primary_force + @secondary_force) / @mass
-      result += primary_vel * secondary_time + calculate_distance(secondary_acc, secondary_time)
-    end
-    result
+    return calculate_primary_distance(time) unless time - @delay > 0
+    calculate_primary_distance(time) + calculate_secondaly_distance(time)
   end
 
   private
 
   def calculate_distance(acc, time)
     0.5 * acc * time * time
+  end
+
+  def calculate_primary_distance(time)
+    calculate_distance(@primary_acc, [time, @delay].min)
+  end
+
+  def calculate_secondaly_distance(time)
+    @primary_acc * @delay * (time - @delay) + calculate_distance(@secondary_acc, time - @delay)
   end
 end
