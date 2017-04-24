633e8e910a35cd98ae3dc92fed0a51d64298fa15
ttsuji <ttsuji@aiming-inc.com>
2017-04-24 19:03:53 +0900

    Add 6.10

---
+++ spec/6.10-substitute_algorithm_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+describe Friend do
+  describe '#found_friends' do
+    it 'Friends is only John' do
+      friend = Friend.new
+      result = friend.found_friends(["John"])
+
+      expect(result).to match_array(["John"])
+    end
+
+    it 'Friends are Don and John and Kent' do
+      friend = Friend.new
+      result = friend.found_friends(["Don", "John", "Kent"])
+
+      expect(result).to match_array(["Don", "John", "Kent"])
+    end
+
+    it 'Mary is not friend' do
+      friend = Friend.new
+      result = friend.found_friends(["Don", "Mary"])
+
+      expect(result).not_to contain_exactly("Mary")
+    end
+  end
+end
