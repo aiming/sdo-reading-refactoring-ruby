5381477a1f6ddeab8c46589ce45a0d6b98c40b4e
ttsuji <ttsuji@aiming-inc.com>
2017-04-24 20:05:27 +0900

    replace old method to new method

--- lib/6.10-substitute_algorithm.rb
+++ lib/6.10-substitute_algorithm.rb
@@ -1,21 +1,5 @@
 class Friend
   def found_friends(people)
-    friends = []
-    people.each do |person|
-      if person == "Don"
-        friends << "Don"
-      end
-      if person == "John"
-        friends << "John"
-      end
-      if person == "Kent"
-        friends << "Kent"
-      end
-    end
-    return friends
-  end
-
-  def found_friends2(people)
     people.select do |person|
       %w(Don John Kent).include? person
     end
--- spec/6.10-substitute_algorithm_spec.rb
+++ spec/6.10-substitute_algorithm_spec.rb
@@ -22,27 +22,4 @@ describe Friend do
       expect(result).not_to contain_exactly("Mary")
     end
   end
-
-    describe '#found_friends2' do
-    it 'Friends is only John' do
-      friend = Friend.new
-      result = friend.found_friends2(["John"])
-
-      expect(result).to match_array(["John"])
-    end
-
-    it 'Friends are Don and John and Kent' do
-      friend = Friend.new
-      result = friend.found_friends2(["Don", "John", "Kent"])
-
-      expect(result).to match_array(["Don", "John", "Kent"])
-    end
-
-    it 'Mary is not friend' do
-      friend = Friend.new
-      result = friend.found_friends2(["Don", "Mary"])
-
-      expect(result).not_to contain_exactly("Mary")
-    end
-  end
 end
